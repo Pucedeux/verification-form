befce372ccba767512e05c0c2980761c4d2ce318
Pucedeux <159967413+Pucedeux@users.noreply.github.com>
2024-09-10 06:53:26 +0200

    script-verify.js

--- script-verify.js
+++ script-verify.js
@@ -3,14 +3,19 @@ document.getElementById('verificationForm').addEventListener('submit', function(
     const phoneNumber = document.getElementById('phoneNumber').value;
     const name = document.getElementById('name').value;
 
-    // Envoyer les données à un serveur ou les stocker localement
-    console.log('Numéro de téléphone:', phoneNumber);
-    console.log('Nom:', name);
-
-    // Exemple de stockage local (localStorage)
-    let contacts = JSON.parse(localStorage.getItem('contacts')) || [];
-    contacts.push({ phoneNumber, name });
-    localStorage.setItem('contacts', JSON.stringify(contacts));
-
-    alert('Informations vérifiées avec succès !');
+    fetch('https://script.google.com/macros/s/https://script.google.com/macros/s/AKfycbxoZhZWcsSywCpw7iVsNcFtl9ayXe7TzAya2RsWPqHcY6h1JZjj5TfVS44x6p7_sL82/exec/exec', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ phoneNumber, name })
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log('Success:', data);
+        alert('Informations vérifiées avec succès !');
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
 });
